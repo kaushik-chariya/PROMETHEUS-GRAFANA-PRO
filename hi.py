2197acc94e1c240316b9f67d24e87909e5c2536c
kaushik013 <chariyakaushik1435@gmail.com>
2026-05-11 21:21:33 +0530

    hi

---
+++ hi.py
@@ -0,0 +1,12 @@
+a = from dvclive.huggingface import DVCLiveCallback
+
+trainer = Trainer(
+    
+    , args,
+    train_dataset=train_data,
+    eval_dataset=eval_data,
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics,
+)
+trainer.add_callback(DVCLiveCallback(save_dvc_exp=True))
+trainer.train()
